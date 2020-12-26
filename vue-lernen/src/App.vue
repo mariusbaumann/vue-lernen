5ab0f1e326aa33936172880ce0ed33318cd2be3b
Marius Baumann <mariusbaumann@MacBook-Air-von-Marius.local>
2020-12-27 00:23:51 +0100

    clean calendar

---
+++ vue-lernen/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+
+    <CalendarWeek />
+
+    <CalendarEntry />
+
+  </div>
+</template>
+
+<script>
+import CalendarWeek from './components/CalendarWeek.vue';
+import CalendarEntry from './components/CalendarEntry.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CalendarWeek,
+    CalendarEntry
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-lernen/src/data.js
@@ -0,0 +1,58 @@
+export const calendarWeekData = [
+    {
+        id: 1,
+        shortName: 'Mo',
+        fullName: 'Montag',
+        events: [
+            { title: 'Vue.js lernen', edit: false, color: 'success' },
+            { title: 'üben, üben, üben', edit: false, color: 'info' },
+        ],
+        active: false
+    },
+    {
+        id: 2,
+        shortName: 'Di',
+        fullName: 'Dienstag',
+        events: [],
+        active: false
+    },
+    {
+        id: 3,
+        shortName: 'Mi',
+        fullName: 'Mittwoch',
+        events: [],
+        active: true
+    },
+    {
+        id: 4,
+        shortName: 'Do',
+        fullName: 'Donnerstag',
+        events: [
+            { title: 'Grillen mit Freunden', edit: false, color: 'primary' }
+        ],
+        active: false
+    },
+    {
+        id: 5,
+        shortName: 'Fr',
+        fullName: 'Freitag',
+        events: [],
+        active: false
+    },
+    {
+        id: 6,
+        shortName: 'Sa',
+        fullName: 'Samstag',
+        events: [],
+        active: false
+    },
+    {
+        id: 7,
+        shortName: 'So',
+        fullName: 'Sonntag',
+        events: [
+            { title: 'Müll rausbringen', edit: false, color: 'danger' }
+        ],
+        active: false
+    },
+]
